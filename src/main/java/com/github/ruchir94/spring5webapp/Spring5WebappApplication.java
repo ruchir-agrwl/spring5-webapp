419d58bc465db2a73024a71db7d61f0453f41402
Ruchir.Agrawal <>
2019-05-05 01:01:56 +0530

    Init Project

---
+++ src/main/java/com/github/ruchir94/spring5webapp/Spring5WebappApplication.java
@@ -0,0 +1,13 @@
+package com.github.ruchir94.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebappApplication.class, args);
+	}
+
+}
